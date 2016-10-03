63e68e385b2be0bc6294b196c1234260050db0ff
SaltanatShapkhatova <0604shapkhatova@gmail.com>
2016-10-04 00:25:54 +0600

    2nd

---
+++ Desktop/fact.js
@@ -0,0 +1,9 @@
+var n = prompt("Enter");
+var f=1;
+for(var i=1; i<=n; i++){
+	f*=i;
+}
+console.log(f);
+
+
+void factorial 
---
+++ Desktop/portfolio 1/fact.js
@@ -0,0 +1,6 @@
+var n;
+var f=1;
+for(var i=1; i<=5; i++){
+	f*=i;
+}
+console.log(f);
